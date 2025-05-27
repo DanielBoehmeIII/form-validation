5b55fb0eb0c0e5c13bfa754071169f12504da234
DanielBoehmeIII <172210340+DanielBoehmeIII@users.noreply.github.com>
2025-05-27 14:35:02 -0400

    Implement form validation using Javascript
    Use RegExp to validate input
      -     Use checkPostalCode(), checkEmail(), checkPassword(), and
            checkPasswordConfirmation() for changing .error-message elements
      -     Use "altered" attribute to regulate css for elements that haven't
            been interected with

--- src/index.js
+++ src/index.js
@@ -1,113 +1,162 @@
+// Some code borrowed from "https://developer.mozilla.org/en-US/docs/Web/HTML/Guides/Constraint_validation""
 import "./style.css";
-import { bgArray, powerOnSound, powerOffSound } from "./funcs/AssetHandling.js";
-import {
-  createFolder,
-  createApp,
-  toggleDelete,
-  parent,
-} from "./funcs/createDeleteFuncs.js";
-import { beginLocalStorage } from "./funcs/localStorage.js";
-export let usernameValue = "Username";
-const grandparent = document.getElementById("content");
-parent.id = "pc-box";
-let bgImg = document.createElement("img");
-bgImg.id = "bgImg";
-bgImg.src = bgArray[0];
-grandparent.appendChild(parent);
-parent.appendChild(bgImg);
-// Create the app instance and append it after initialization
-const createAppElement = createApp("create-new-app", null);
-createAppElement.elementOnDOM.addEventListener("click", (e) => createFolder());
-export const deleteAppElement = createApp("delete-app", null);
-deleteAppElement.elementOnDOM.addEventListener("click", () => {
-  toggleDelete();
-});
-window.onload = () => {
-  beginLocalStorage();
-};
 
-const leftArrow = document.getElementById("left-arrow");
-const rightArrow = document.getElementById("right-arrow");
-let bgNum = 0;
+// --- DOM Elements ---
+const country = document.getElementById("country");
+const postalCodeField = document.getElementById("postal-code");
+const emailField = document.getElementById("email");
+const passwordField = document.getElementById("password");
+const passwordConfirmationField = document.getElementById(
+  "password-confirmation",
+);
 
-leftArrow.addEventListener("click", () => {
-  if (bgNum === 0) {
-    bgNum = bgArray.length - 1;
+// --- Postal Code Validation ---
+function checkPostalCode() {
+  const errorSpan = document.getElementById("postal-error");
+  const constraints = {
+    us: [
+      "^(US-)?\\d{5}$",
+      "American postal codes must have exactly 5 digits: e.g. US-19130 or 19130",
+    ],
+    uk: [
+      "^(UK-)?[A-Z0-9]{5,7}$",
+      "English postal codes must have 5 to 7 Alphanumerical characters",
+    ],
+    fr: [
+      "^(F-)?\\d{5}$",
+      "French postal codes must have exactly 5 digits: e.g. F-79023 or 79023",
+    ],
+    de: [
+      "^(D-)?\\d{5}$",
+      "German postal codes must have exactly 5 digits: e.g. D-53412 or 53412",
+    ],
+    cn: [
+      "^(CN-)?\\d{6}$",
+      "Chinese postal codes must have exactly 6 digits: e.g. CN-694132 or 694132",
+    ],
+  };
+
+  if (country.value !== "") {
+    const countryValue = country.value;
+    const constraint = new RegExp(constraints[countryValue][0], "");
+    postalCodeField.setAttribute("altered", true);
+
+    if (constraint.test(postalCodeField.value)) {
+      postalCodeField.setCustomValidity("");
+    } else {
+      postalCodeField.setCustomValidity(constraints[countryValue][1]);
+    }
+    country.setAttribute("altered", true);
   } else {
-    --bgNum;
+    country.setAttribute("altered", false);
   }
-  changeBg(bgNum);
-  localStorage.setItem("bgImgIndex", bgNum);
-});
 
-rightArrow.addEventListener("click", () => {
-  if (bgNum === bgArray.length - 1) {
-    bgNum = 0;
+  errorSpan.textContent = postalCodeField.validationMessage;
+}
+
+// --- Email Validation ---
+function checkEmail() {
+  const errorSpan = document.getElementById("email-error");
+  const pattern = "^[^@]{1,256}@[a-zA-Z0-9.-]{1,60}\\.[a-z]{2,6}$";
+  const messageFormat = [
+    "Please enter an email with an @ symbol",
+    "Please include a top-level domain",
+  ];
+  const constraint = new RegExp(pattern, "i");
+
+  if (emailField.value !== "") {
+    emailField.setAttribute("altered", true);
+    if (constraint.test(emailField.value)) {
+      emailField.setCustomValidity("");
+    } else if (!emailField.value.includes("@")) {
+      emailField.setCustomValidity(messageFormat[0]);
+    } else {
+      emailField.setCustomValidity(messageFormat[1]);
+    }
+    errorSpan.textContent = emailField.validationMessage;
   } else {
-    ++bgNum;
-  }
-  changeBg(bgNum);
-  localStorage.setItem("bgImgIndex", bgNum);
-});
-
-export function changeBg(index) {
-  bgImg.src = bgArray[index];
-}
-
-let powerOn = true;
-
-async function turnOn() {
-  powerOnSound.play();
-  parent.style.animation = "scale-in 1s ease-in-out 1";
-  grandparent.appendChild(parent);
-  await sleep(1000);
-}
-
-async function turnOff() {
-  powerOffSound.play();
-  parent.style.animation = "scale-out 1s ease-in-out 1";
-  await sleep(1000);
-  grandparent.removeChild(parent);
-}
-
-const power = document.getElementById("power");
-power.addEventListener("click", () => {
-  if (powerOn) {
-    turnOff();
-  } else {
-    turnOn();
-  }
-  powerOn = !powerOn;
-});
-
-function sleep(ms) {
-  return new Promise((r) => setTimeout(r, ms));
-}
-
-const usernameDisplay = document.getElementById("username-display");
-const usernameContainer = document.getElementById("username-container");
-let usernameInput = document.createElement("input");
-usernameInput.id = "username-input";
-
-usernameDisplay.addEventListener("click", (e) => {
-  changeUser();
-});
-
-export function changeUser(name) {
-  if (name == null) {
-    usernameContainer.removeChild(usernameDisplay);
-    usernameContainer.appendChild(usernameInput);
-    usernameInput.addEventListener("keydown", (e) => {
-      if (e.key === "Enter") {
-        usernameValue = usernameInput.value;
-        usernameDisplay.textContent = `${usernameValue}`;
-        usernameContainer.removeChild(usernameInput);
-        usernameContainer.appendChild(usernameDisplay);
-        localStorage.setItem("username", usernameValue);
-      }
-    });
-  } else {
-    usernameDisplay.textContent = `${name}`;
-    usernameValue = name;
+    emailField.setAttribute("altered", false);
   }
 }
+
+// --- Password Validation ---
+function checkPassword() {
+  const errorSpan = document.getElementById("password-error");
+  const pattern = "^[a-zA-Z0-9.-!@#$%^&*()_+=]{12,128}";
+  const messageFormat = [
+    "Please enter a password between 12 and 128 characters",
+    "Please enter a password with a special character",
+  ];
+
+  if (passwordField.value !== "") {
+    passwordField.setAttribute("altered", true);
+    if (passwordField.value.length < 12 || passwordField.value.length > 128) {
+      passwordField.setCustomValidity(messageFormat[0]);
+      passwordConfirmationField.setCustomValidity("");
+    } else if (!/[!@#$%^&*()_\-+=]/.test(passwordField.value)) {
+      passwordField.setCustomValidity(messageFormat[1]);
+      passwordConfirmationField.setCustomValidity("");
+    } else {
+      passwordField.setCustomValidity("");
+      passwordConfirmationField.setCustomValidity("");
+    }
+    passwordField.setAttribute("altered", true);
+    errorSpan.textContent = passwordField.validationMessage;
+  } else {
+    passwordField.setAttribute("altered", false);
+  }
+}
+
+// --- Password Confirmation ---
+function checkPasswordConfirmation() {
+  const errorSpan = document.getElementById("password-confirmation-error");
+  const message = [
+    "Please confirm your password",
+    "Password confirmation doesn't match, please try again",
+  ];
+  if (
+    passwordField.value !== passwordConfirmationField.value &&
+    passwordConfirmationField.value === "" &&
+    passwordConfirmationField.getAttribute("altered") === "true"
+  ) {
+    passwordConfirmationField.setAttribute("altered", true);
+    passwordField.setCustomValidity("");
+    passwordConfirmationField.setCustomValidity(message[0]);
+    errorSpan.textContent = passwordConfirmationField.validationMessage;
+  } else if (
+    passwordField.value !== passwordConfirmationField.value &&
+    passwordConfirmationField.value !== ""
+  ) {
+    passwordField.setCustomValidity("");
+    passwordConfirmationField.setCustomValidity(message[1]);
+    passwordConfirmationField.setAttribute("altered", true);
+    errorSpan.textContent = passwordConfirmationField.validationMessage;
+  } else {
+    passwordField.setCustomValidity("");
+    passwordConfirmationField.setCustomValidity("");
+    passwordConfirmationField.setAttribute("altered", false);
+    errorSpan.textContent = passwordConfirmationField.validationMessage;
+  }
+}
+
+// --- Event Listeners ---
+country.addEventListener("input", checkPostalCode); // re-check postal when country changes
+postalCodeField.addEventListener("input", checkPostalCode);
+emailField.addEventListener("input", checkEmail);
+passwordField.addEventListener("input", checkPassword);
+passwordConfirmationField.addEventListener("input", checkPasswordConfirmation);
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (e) => {
+  checkPostalCode();
+  checkEmail();
+  checkPassword();
+  checkPasswordConfirmation();
+
+  if (!form.checkValidity()) {
+    e.preventDefault();
+    alert("invalid");
+  } else {
+    alert("valid");
+  }
+});
